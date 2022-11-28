89e2c35a667ceb8d70f8612ac6f94b7945ea10a2
Abdulvaris <94841698+abdulvaris02@users.noreply.github.com>
2022-11-28 11:01:36 +0500

    Update my_nba_game_analysis.py

--- my_nba_game_analysis.py
+++ my_nba_game_analysis.py
@@ -16,7 +16,8 @@ def find_players(play_by_play_moves):
     for i in range(len(result)):
         for a in range(len(result[i])):
             if(result[i][a].endswith(".") and (result[i][a]+ " " + result[i][a+1]) not in ans):
-                    ans.append(result[i][a] + " " + result[i][a+1]) 
+                    ans.append(result[i][a] + " " + result[i][a+1])
+    # print(*ans, sep = '\n')
     return ans
 
 def make_players_statistics(play_by_play_moves, lst_of_players):
@@ -30,27 +31,30 @@ def make_players_statistics(play_by_play_moves, lst_of_players):
         
         for play in play_by_play_moves:
             
-            match_fg = re.search(r"(\w\. \w+) makes \d-pt", play[-1])
+            match_fg = re.search(r"(\w\. \w+) makes 2-pt", play[-1])
             if match_fg: 
                 if(match_fg.group(1) == player):  
                     player_static['FG'] += 1
                     player_static['FGA'] += 1
             
-            match_fga = re.search(r"(\w\. \w+) misses \d-pt", play[-1])
+            match_fga = re.search(r"(\w\. \w+) misses 2-pt", play[-1])
             if match_fga:
                 if(match_fga.group(1) == player):
                     player_static['FGA'] += 1
-            
+
             match_3p = re.search(r"(\w. \w+) makes 3-pt", play[-1])
             if match_3p:
                 if(match_3p.group(1) == player):
                     player_static['3P'] += 1
                     player_static['3PA'] += 1
+                    player_static['FG'] += 1
+                    player_static['FGA'] += 1
             
             match_3pa = re.search(r"(\w. \w+) misses 3-pt", play[-1])
             if match_3pa:
                 if(match_3pa.group(1) == player):
                     player_static['3PA'] += 1
+                    player_static['FGA'] += 1
             
             match_ft = re.search(r"(\w\. \w+) makes free throw", play[-1])
             if(match_ft): 
@@ -61,7 +65,6 @@ def make_players_statistics(play_by_play_moves, lst_of_players):
             match_ft_a = re.search(r"(\w\. \w+) misses free throw", play[-1])
             if(match_ft_a):
                 if(match_ft_a.group(1) == player):
-                    player_static['FG'] += 1
                     player_static['FTA'] += 1 
                   
             match_orb = re.search(r"Offensive rebound by (\w\. \w+)", play[-1])
@@ -102,23 +105,23 @@ def make_players_statistics(play_by_play_moves, lst_of_players):
                     player_static['PF'] += 1
         lst_fg.append(player_static['FG'])
         lst_fga.append(player_static['FGA'])
-        try:
-            player_static['FG%'] = round(player_static['FG'] / player_static['FGA'], 3)
-        except:
-            player_static['FG%'] = " "
+        if(player_static['FGA'] != 0):
+            player_static['FG%'] = round((player_static['FG'] / player_static['FGA']), 3)
+        else:
+            player_static['FG%'] = 0
         
-        try:
-            player_static['3P%'] = round(player_static['3P'] / player_static['3PA'], 3)
-        except:
-            player_static['3P%'] = " " 
+        if(player_static['3PA'] != 0):
+            player_static['3P%'] = round((player_static['3P'] / player_static['3PA']), 3)
+        else:
+            player_static['3P%'] = 0 
 
-        try:
-            player_static['FT%'] = round(player_static['FT'] / player_static['FTA'], 3)
-        except:
-            player_static['FT%'] = " "
+        if(player_static['FTA'] != 0):
+            player_static['FT%'] = round((player_static['FT'] / player_static['FTA']), 3)
+        else:
+            player_static['FT%'] = 0
         
         if player_static["FG"] != 0:
-            player_static["PTS"] = 2*(player_static["FG"] - player_static["3P"]) + 3*(player_static["3P"] + player_static["FT"])
+            player_static["PTS"] = 2*(player_static['FG'] - player_static['3P']) + 3*(player_static['3P']) + player_static['FT']
         else:
             player_static["PTS"] = 0   
        
@@ -127,6 +130,17 @@ def make_players_statistics(play_by_play_moves, lst_of_players):
     return statics_of_all_players
 
 def team_matching(play_by_play_moves, lst_of_players, statics):
+    team_1_name = play_by_play_moves[0][2]
+    team_i = 1
+    while team_i != len(play_by_play_moves):
+        if(team_1_name != play_by_play_moves[team_i][2]):
+            team_2_name = play_by_play_moves[team_i][2]
+            break
+        team_i += 1
+
+    # print(team_1_name, "\n\n ", team_2_name)
+    # print("\n\n\n\n\n\n\n\n")
+
     teams_of_players = {name: '' for name in lst_of_players}
     total1 = {"total1": 'Team Totals', "FG": 0, "FGA": 0, "FG%": 0, "3P": 0, "3PA": 0, "3P%": 0, "FT": 0, "FTA": 0, "FT%": 0, "ORB": 0, "DRB": 0, "TRB": 0, "AST": 0, "STL": 0, "BLK": 0, "TOV": 0, "PF": 0, "PTS": 0}
     total2 = {"total2": 'Team Totals', "FG": 0, "FGA": 0, "FG%": 0, "3P": 0, "3PA": 0, "3P%": 0, "FT": 0, "FTA": 0, "FT%": 0, "ORB": 0, "DRB": 0, "TRB": 0, "AST": 0, "STL": 0, "BLK": 0, "TOV": 0, "PF": 0, "PTS": 0}
@@ -150,11 +164,17 @@ def team_matching(play_by_play_moves, lst_of_players, statics):
         if(match_df):
             name = match_df.group(1)
             teams_of_players[name] = play[2]
+
+        match_df = re.search(r"Defensive rebound by (\w\. \w+-\w+)", play[-1])
+        if(match_df):
+            name = match_df.group(1)
+            teams_of_players[name] = play[2]
+            
     team1 = []
     team2 = []
     print(*statics[0].keys(), sep = '\t')
     for k,v in teams_of_players.items():
-        if(v == "GOLDEN_STATE_WARRIORS"):
+        if(v == team_2_name):
             for event in statics:
                 if(event['Players'] == k):
                     team1.append(event)
@@ -168,14 +188,26 @@ def team_matching(play_by_play_moves, lst_of_players, statics):
                     try:
                         total1[k] += v
                     except:
-                        continue         
-            
+                        continue
+            try:             
+                total1["FG%"] = round((total1["FG"]/total1["FGA"]), 3)
+            except:
+                continue 
+            try:
+                total1["3P%"] = round((total1["3P"]/total1["3PA"]), 3)
+            except:
+                continue
+            try:
+                total1["FT%"] = round((total1["FT"]/total1["FTA"]), 3)                    
+            except:
+                continue
+
     print(*total1.values(), sep='\t')
     print("\n\n")
       
     print(*statics[0].keys(), sep = '\t')
     for k,v in teams_of_players.items():
-        if(v == "OKLAHOMA_CITY_THUNDER"):
+        if(v == team_1_name):
             for event in statics:
                 if(event['Players'] == k):
                     team2.append(event)
@@ -199,5 +231,3 @@ def _main():
     statics = make_players_statistics(play_by_play_moves, lst_of_players)
     teams = team_matching(play_by_play_moves, lst_of_players, statics)
 _main()
-            
-   
